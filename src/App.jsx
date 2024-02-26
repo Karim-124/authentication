663ad64233e41b750f97d7f61a8d43fecad2e641
karim Abdelazim <ka1465@fayoum.edu.eg>
2024-02-27 01:45:04 +0200

    use react router dom - create login componet.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Login from './component/Login'
 
 function App() {
 
 
   return (
     <>
-      
+      <Routes>
+        <Route path='/login' element={ <Login/>} />
+      </Routes>
     </>
   )
 }
