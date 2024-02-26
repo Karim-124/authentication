78f12853af1710336bf38cf7cef6dce8e74816cc
karim Abdelazim <ka1465@fayoum.edu.eg>
2024-02-27 01:25:57 +0200

    install firebase and put the code inside firebase.js.

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAJpX0j9GXXAOf07d0MosCFEatXWsnvges",
+    authDomain: "authproject-2ad9d.firebaseapp.com",
+    projectId: "authproject-2ad9d",
+    storageBucket: "authproject-2ad9d.appspot.com",
+    messagingSenderId: "122551041209",
+    appId: "1:122551041209:web:619781dd300274037d2f68",
+    measurementId: "G-GEZB4CJ6V4"
+};
