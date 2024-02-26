e0a1240156922252723171d663c226a330a44c40
karim Abdelazim <ka1465@fayoum.edu.eg>
2024-02-27 01:35:56 +0200

    Make protected authentication.

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,9 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAJpX0j9GXXAOf07d0MosCFEatXWsnvges",
-    authDomain: "authproject-2ad9d.firebaseapp.com",
-    projectId: "authproject-2ad9d",
-    storageBucket: "authproject-2ad9d.appspot.com",
-    messagingSenderId: "122551041209",
-    appId: "1:122551041209:web:619781dd300274037d2f68",
-    measurementId: "G-GEZB4CJ6V4"
+    apiKey: process.env.REACT_APP_FIREBSE_KEY,
+    authDomain: process.env.REACT_APP_FIREBSE_Domain,
+    storageBucket: process.env.REACT_APP_FIREBSE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBSE_MESSAGE_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBSE_APP_ID,
+ 
 };
