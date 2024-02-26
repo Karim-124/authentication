873705e148bbb6aecda593504eb35116ab54590d
karim Abdelazim <ka1465@fayoum.edu.eg>
2024-02-27 01:49:39 +0200

    Create Update Profile componet - make routing for this compoent.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import Login from './component/Login'
 import SiginUp from './component/SiginUp';
 import ForgetPassword from './component/ForgetPassword';
+import UpdateProfile from './component/UpdateProfile';
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
         <Route path='/login' element={<Login />} />
         <Route path='/signup' element={<SiginUp />} />
         <Route path='/forget-password' element={<ForgetPassword />} />
+        <Route path='/update-profile' element={<UpdateProfile />} />
       </Routes>
     </>
   )
