7930f291533d29885210abb6901da85b4181fbca
karim Abdelazim <ka1465@fayoum.edu.eg>
2024-02-27 02:22:14 +0200

    Edit The Form Of Input Filed In Sigin up.

--- src/component/SiginUp.jsx
+++ src/component/SiginUp.jsx
@@ -5,22 +5,25 @@ function SiginUp() {
     <>
 
       <div className="flex justify-center items-center h-screen">
-        <form className="w-full max-w-md bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <form className="w-full max-w-md bg-white shadow-lg rounded px-8 pt-6 pb-8 mb-4">
+          <h2 className="text-center text-xl font-bold mb-4">Sign Up</h2>
           <div className="mb-4">
             <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">Email</label>
-            <input type="text" id="website-admin" class="rounded-none rounded-e-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Bonnie Green" />
+            <input type="text" id="email" class="rounded bg-white border border-sky-400 block w-full text-sm p-2.5  " placeholder="Enter your email" />
           </div>
           <div className="mb-4">
             <label htmlFor="password" className="block text-gray-700 text-sm font-bold mb-2">Password</label>
-            <input type="password" id="password" className="input-field" placeholder="Enter your password" />
+            <input type="password" id="password" class="rounded bg-white border border-sky-400 block w-full text-sm p-2.5" placeholder="Enter your password" />
           </div>
           <div className="mb-6">
             <label htmlFor="confirm-password" className="block text-gray-700 text-sm font-bold mb-2">Confirm Password</label>
-            <input type="password" id="confirm-password" className="input-field" placeholder="Confirm your password" />
+            <input type="password" id="confirm-password" class="rounded bg-white border border-sky-400 	 block w-full text-sm p-2.5" placeholder="Confirm your password" />
           </div>
           <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Sign Up</button>
         </form>
       </div>
+
+
 
     </>
   )
